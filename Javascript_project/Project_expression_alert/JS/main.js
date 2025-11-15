a07539dbe3b7fabbf5e81fea068560a6a60cd4cc
christopher son lopez <christophersonlopez@gmail.com>
2025-11-14 20:45:09 -0500

    i finish my assignment for my expression concatenating two variable

--- Javascript_project/Project_expression_alert/JS/main.js
+++ Javascript_project/Project_expression_alert/JS/main.js
@@ -5,7 +5,7 @@ let variablename="This is my variable assignment <br>";
 
 document.write(variablename);
 
-/*this is my string variable fro my assignment */
+//this is my string variable fro my assignment 
 
 
 let stringvariable="This is my string variable assignment<br>";
@@ -13,15 +13,15 @@ let stringvariable="This is my string variable assignment<br>";
 window.alert(stringvariable);
 
 
-/** this is for my escape character assignment*/
+//this is for my escape character assignment
 
 document.write("I don\'t want to see you! <br><br>");
 
-/** this is for my concatenation assignment*/
+// this is for my concatenation assignment
 let name="God";
  document.write(name+" is good all the time<br><br>")
 
- /** this is for my assign a Multiple Variable assignment*/
+ // this is for my assign a Multiple Variable assignment
 
  let family="Lopez<br>",Father="Christopher Son<br>", Mother="Zarine<br>",Son="Zander<br><br>";
 
@@ -29,10 +29,27 @@ let name="God";
  document.write("My Family name is "+family +" Dad name is "+Father+" Mom name is "+Mother+" My son name is "+Son+"<br><br><br>");
 
 
-  /** this is for my assign a Expression Variable assignment*/
+  //this is for my assign a Expression Variable assignment
 
 
   let num1=1;
   let num2=5;
   let result=num1+num2;
-  document.write("The sum of num1 value of 1 and num2 and value of 5 is "+result);
+  document.write("The sum of num1 value of 1 and num2 and value of 5 is "+result+"<br><br><br>");
+
+  //this is for the windows alert and document.write to display atext
+
+  window.alert("This is my Assignment");
+  document.write("I used ah document.write in this code<br>");
+
+  //this is for concatenating the two variable
+
+  let name1="This is the beginning of the string <br>";
+  let name2="and this is the end of the string <br>";
+  let show=name1+name2;
+  document.write(show);
+
+  //this is for expression assignment
+let result1=(5*6);
+  
+  document.write("Answer is "+(result1));
