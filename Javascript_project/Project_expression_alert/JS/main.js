20f94d10d061eb14fbb35da75c34ea729e1dc361
christopher son lopez <christophersonlopez@gmail.com>
2025-11-14 20:20:43 -0500

    add a expression assignment

--- Javascript_project/Project_expression_alert/JS/main.js
+++ Javascript_project/Project_expression_alert/JS/main.js
@@ -26,4 +26,13 @@ let name="God";
  let family="Lopez<br>",Father="Christopher Son<br>", Mother="Zarine<br>",Son="Zander<br><br>";
 
 
- document.write("My Family name is "+family +" Dad name is "+Father+" Mom name is "+Mother+" My son name is "+Son )
+ document.write("My Family name is "+family +" Dad name is "+Father+" Mom name is "+Mother+" My son name is "+Son+"<br><br><br>");
+
+
+  /** this is for my assign a Expression Variable assignment*/
+
+
+  let num1=1;
+  let num2=5;
+  let result=num1+num2;
+  document.write("The sum of num1 value of 1 and num2 and value of 5 is "+result);
