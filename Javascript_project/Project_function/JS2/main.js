0ace0d6181b68d0c9227369db212e56980300064
christopher son lopez <christophersonlopez@gmail.com>
2025-11-14 22:43:21 -0500

    adding a function in use a += operator

--- Javascript_project/Project_function/JS2/main.js
+++ Javascript_project/Project_function/JS2/main.js
@@ -1,5 +1,11 @@
+//this line of code using two variable in display using a function
 function itsme(){
     let nameko="Christopher Son"
     let lastname="Lopez"
     document.getElementById("callme").innerHTML=("My name is "+nameko+" "+lastname);
+}
+
+//this line of code is adding or concatenating a string
+function addtext(){
+    document.getElementById("concat").innerHTML+=" Christopher";
 }
