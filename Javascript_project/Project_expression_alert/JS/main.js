00bda4286dd954c58fd98da2401bc42cac2d8818
christopher son lopez <christophersonlopez@gmail.com>
2025-11-14 19:55:38 -0500

    added a  escape character assignment

--- Javascript_project/Project_expression_alert/JS/main.js
+++ Javascript_project/Project_expression_alert/JS/main.js
@@ -1,7 +1,7 @@
 window.alert("hello Christopher Son");
-document.write("Hello Christ ");
+document.write("Hello Christ <br>");
 
-let variablename="This is my variable assignment";
+let variablename="This is my variable assignment <br>";
 
 document.write(variablename);
 
@@ -11,3 +11,8 @@ document.write(variablename);
 let stringvariable="This is my string variable assignment";
 
 window.alert(stringvariable);
+
+
+/** this is for my escape character assignment*/
+
+document.write("I don't want to see you!");
