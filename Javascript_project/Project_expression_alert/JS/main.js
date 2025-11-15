f15537bbebc4ed10a58e52b757df36c9f186f2f9
christopher son lopez <christophersonlopez@gmail.com>
2025-11-14 19:44:42 -0500

    i create a string variable

--- Javascript_project/Project_expression_alert/JS/main.js
+++ Javascript_project/Project_expression_alert/JS/main.js
@@ -1,1 +1,13 @@
-window.alert("hello Christopher Son")
+window.alert("hello Christopher Son");
+document.write("Hello Christ ");
+
+let variablename="This is my variable assignment";
+
+document.write(variablename);
+
+/*this is my string variable fro my assignment */
+
+
+let stringvariable="This is my string variable assignment";
+
+window.alert(stringvariable);
