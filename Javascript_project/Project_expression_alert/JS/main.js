55e43875f998e4bf1cf6fc5aa9c9f91645afb2e6
christopher son lopez <christophersonlopez@gmail.com>
2025-11-14 20:08:19 -0500

    created a multiple variable

--- Javascript_project/Project_expression_alert/JS/main.js
+++ Javascript_project/Project_expression_alert/JS/main.js
@@ -8,11 +8,22 @@ document.write(variablename);
 /*this is my string variable fro my assignment */
 
 
-let stringvariable="This is my string variable assignment";
+let stringvariable="This is my string variable assignment<br>";
 
 window.alert(stringvariable);
 
 
 /** this is for my escape character assignment*/
 
-document.write("I don't want to see you!");
+document.write("I don\'t want to see you! <br><br>");
+
+/** this is for my concatenation assignment*/
+let name="God";
+ document.write(name+" is good all the time<br><br>")
+
+ /** this is for my assign a Multiple Variable assignment*/
+
+ let family="Lopez<br>",Father="Christopher Son<br>", Mother="Zarine<br>",Son="Zander<br><br>";
+
+
+ document.write("My Family name is "+family +" Dad name is "+Father+" Mom name is "+Mother+" My son name is "+Son )
