f4902aa34bd48a220a627086f16b6fbd0e894004
christopher son lopez <christophersonlopez@gmail.com>
2025-11-15 23:02:13 -0500

    added a function for my subtraction

--- Javascript_project/Project_math_operator/JS/math.js
+++ Javascript_project/Project_math_operator/JS/math.js
@@ -1,3 +1,4 @@
+// function for addition
 function addnumber(num1,num2){
     return num1+num2;
 
@@ -5,4 +6,14 @@ function addnumber(num1,num2){
 function compute(){
     let result=addnumber(10,10);
     document.getElementById("adding").innerHTML=("The sum of 10+10 is ="+result);
+}
+
+// function for subtraction
+function subtractnumber(num1,num2){
+    return num1-num2;
+
+}
+function compute1(){
+    let result=subtractnumber(20,10);
+    document.getElementById("subtractanumber").innerHTML=("The subtraction result of 20+10 is ="+result);
 }
