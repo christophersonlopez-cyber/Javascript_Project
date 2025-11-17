a4c76fec404131523e1ab080c2aeb8f0c3b5ae56
christopher son lopez <christophersonlopez@gmail.com>
2025-11-16 22:47:16 -0500

    added a folder for dictionary in create a sample dictionary

---
+++ Javascript_project/Project4_dictionaries/JS/dictio.js
@@ -0,0 +1,7 @@
+function dictionary(){
+    let person={name:"Christopher Son",
+        age:25,
+        hobby:"Programming"
+    };
+    document.getElementById("dictiosample").innerHTML=("Hobby: "+person.hobby);
+}
