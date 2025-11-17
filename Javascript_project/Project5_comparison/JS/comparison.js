73d58d1d58cd0e7ed9aa352351729518c60a444c
christopher son lopez <christophersonlopez@gmail.com>
2025-11-16 23:05:13 -0500

    added a coersion

--- Javascript_project/Project5_comparison/JS/comparison.js
+++ Javascript_project/Project5_comparison/JS/comparison.js
@@ -1,4 +1,12 @@
+//sample for comparison
 function comparison(){
     let name="christopher"
     document.getElementById("comparisonsample").innerHTML=(typeof name);
+}
+//function for coersion 
+function coersion(){
+    let name="christopher";
+    let number=10;
+    let result=name+number;
+    document.getElementById("coersionsample").innerHTML=(result);
 }
