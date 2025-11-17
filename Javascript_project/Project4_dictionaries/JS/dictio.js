6f9cb03ed97011bdd13f1853bba3d86a1beae887
christopher son lopez <christophersonlopez@gmail.com>
2025-11-16 22:50:56 -0500

    create a delete function

--- Javascript_project/Project4_dictionaries/JS/dictio.js
+++ Javascript_project/Project4_dictionaries/JS/dictio.js
@@ -3,5 +3,6 @@ function dictionary(){
         age:25,
         hobby:"Programming"
     };
+    delete person.hobby;
     document.getElementById("dictiosample").innerHTML=("Hobby: "+person.hobby);
 }
