b2bfdd44eecda2be5405cd6b77b6d750371a8018
christopher son lopez <christophersonlopez@gmail.com>
2025-11-17 22:56:48 -0500

    create isnan assignment and boolean assignment

--- Javascript_project/Project5_coersion/JS/coersion.js
+++ Javascript_project/Project5_coersion/JS/coersion.js
@@ -10,3 +10,19 @@ function coersion(){
     let result=name+number;
     document.getElementById("coersionsample").innerHTML=(result);
 }
+
+
+function nan(){
+    document.getElementById("nansample").innerHTML= isNaN ("90");
+}
+
+function infinitesam(){
+    document.getElementById("infinitesample").innerHTML=-3E310;
+}
+
+function booleansam(){
+    let number1=10;
+    let number2=2;
+    let result=number1>number2;
+    document.getElementById("booleansample").innerHTML=("Number 10 > 2 = "+result);
+}
