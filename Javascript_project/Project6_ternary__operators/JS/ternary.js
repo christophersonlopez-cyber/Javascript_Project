34208dc203071652fa7fcdd6dce6ed1d5c7a7ff7
christopher son lopez <christophersonlopez@gmail.com>
2025-11-18 17:43:22 -0500

    added a function about ternary can display if able to vote or not

---
+++ Javascript_project/Project6_ternary__operators/JS/ternary.js
@@ -0,0 +1,14 @@
+function ternary(){
+    let height,canride;
+    height=document.getElementById("height").value;
+    canride=(height<53 ? "Your are too short":"Your are tall enough");
+    document.getElementById("ride").innerHTML=canride+"To Ride!"
+}
+
+
+function voting(){
+    let age,canvote;
+    age=document.getElementById("age").value;
+    canvote=(age<18 ? "Your are not able to vote its too young":"Your are able to vote youre in legal age");
+    document.getElementById("agevote").innerHTML=canvote+"To Vote!"
+}
